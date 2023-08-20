cccfd50d19ec7c4aaf7715a6d778546bfe8a6a39
Mahfuj Munna <mahfujmunna17@gmail.com>
2023-08-20 20:15:55 +0600

    messages added

--- index.js
+++ index.js
@@ -121,6 +121,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/messages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await messagesCollection.deleteOne(query);
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
